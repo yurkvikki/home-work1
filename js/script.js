654050bd97bcc1757316a26b483b7a8a89d39146
Viktoriya Yurkova <vika.savosteeva@mail.ru>
2022-08-28 16:16:21 +0300

    init progect

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+/*-- 1. --*/
+/* const youreName = prompt("inter youre name");
+ console.log(youreName); */
+/*-- 2.-- */
+/* const number1= +prompt('inter first number')
+const number2= +prompt('inter second number')
+const number3= +prompt('inter third number')
+let sum=alert(number1+number2+number3)
+console.log(sum) */
+ /*-- 3.-- */
+/*  const userName=prompt('inter youre name')
+ const userNameLast=prompt('inter youre last name')
+ const userAge=+prompt('inter youre age')
+
+if(userAge>17){
+    console.log('Welcome'+","+userName)
+} else{
+    console.log('Sorry, '+userName+' ,you so young')
+} */
+/*-- 4-- */
+/*  const nameBlock= confirm("Хотите ввести название блока?")
+if(nameBlock===false){
+console.log('Элементу не присвоен класс')
+} else{
+    const enterNameBlock= prompt('Введите название блока')
+}
+const nameElement= confirm("Хотите ввести название элемента?")
+if(nameElement===false){
+console.log(nameBlock)
+} else{
+    const enterNameElement= prompt('Введите название элемента')
+}
+const nameMod= confirm("Хотите ввести название модификатора?")
+if(nameElement===true){
+    const enterNameMod= prompt('Введите название модификатора')
+console.log( nameBlock + nameElement + enterNameMod)
+} else{
+    console.log(nameBlock + nameElement)
+}  */
+/* -- 5 -- */
+/* const number1= +prompt('inter first number')
+const number2= +prompt('inter second number')
+const option = prompt(' выберите нужную арифметическую операцию (div, plus, mult, minus)')
+if(option===div){
+console.log(number1/number2)
+} */
